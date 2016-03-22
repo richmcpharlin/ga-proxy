e443979214b8ef40993884edd1c55a3ae77f4b9b
richmcpharlin <rich@richmcpharlin.com>
2016-03-22 14:31:39 +1100

    Addition of config file for setting basic variables

---
+++ config.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+//Set the location from where requests will be sent and recevied from
+$source = 'http://www.richmcpharlin.com';
+//Timeout - period to store files for (in seconds)
+$timeout = 300;
+
+
+?>
